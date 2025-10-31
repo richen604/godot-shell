0fd2193b58cc6f2e11c971b02444fec0cd1fe57a
richen604 <56615615+richen604@users.noreply.github.com>
2025-10-30 19:40:42 -0700

    refactor: removed 2d template

--- flake.nix
+++ flake.nix
@@ -14,10 +14,6 @@
           path = ./templates/project-3d;
           description = "A basic Godot project";
         };
-        project-2d = {
-          path = ./templates/project-2d;
-          description = "A basic Godot 2d project";
-        };
         project-3d = {
           path = ./templates/project-3d;
           description = "A basic Godot 3d project";
