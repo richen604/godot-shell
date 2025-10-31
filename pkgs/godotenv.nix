9fe04f259194234c34e9c8d5731ff2d9ca6dd90c
richen604 <56615615+richen604@users.noreply.github.com>
2025-10-30 22:20:37 -0700

    fix: linked godotenv bin

--- pkgs/godotenv.nix
+++ pkgs/godotenv.nix
@@ -23,6 +23,10 @@ buildDotnetModule rec {
   dotnet-sdk = dotnetCorePackages.sdk_8_0;
   dotnet-runtime = dotnetCorePackages.runtime_8_0;
 
+  postFixup = ''
+    ln -s $out/bin/Chickensoft.GodotEnv $out/bin/godotenv
+  '';
+
   meta = with lib; {
     description = "Manage Godot versions and addons from the command line on Windows, macOS, and Linux.";
     homepage = "https://github.com/chickensoft-games/GodotEnv";
